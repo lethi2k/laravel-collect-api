a977b0f8c8d91a32c72fbf165a1463e39398c8cf
lethi2k <ledinhthi2909@gmail.com>
2023-05-23 23:02:39 +0700

    update api vietcombank

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -8,8 +8,30 @@ class CurrencyController extends BaseController
 {
     public function index()
     {
-        $response = Http::get('https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx');
+        $host = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx";
+        $json_string = $this->getSslPage($host);
+        $result_array = json_decode($json_string, TRUE);
+        foreach ($result_array['Exrate'] as $country_curency) {
+            $list_curency[$country_curency['@attributes']['CurrencyCode']] = $country_curency['@attributes']['Sell'];
+        }
 
-        return $this->responseSuccess($response, 'get data successfully!');
+        return $this->responseSuccess($list_curency, 'get data vietcommbank successfully!');
+    }
+
+
+    function getSslPage($url)
+    {
+        $ch = curl_init();
+        $headr = array();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'spider');
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $xml = simplexml_load_string($data);
+        return json_encode($xml);
     }
 }
